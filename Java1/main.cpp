d62c8c4dc8bbf100d70f05de151d5a590e8af73b
OlisJ <olisj794@gmail.com>
2025-09-26 10:05:12 +0200

    .

---
+++ Java1/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Programimi filloj";
+    cout<<endl;
+    cout<<"Pershendetje \n";
+    cout<<"grupi 4B \n";
+
+    int brinja_a;
+    cout<<"Vendos vleren e brinjes a";
+    cin>>brinja_a;
+
+    int brinja_b;
+    cout<<"Vendos vleren e brinjes b";
+    cin>>brinja_b;
+
+    int rezultati=2*(brinja_a+brinja_b);
+
+    cout<<"Perimetri eshte:"<<rezultati;
+
+
+    return 0; 
+}
