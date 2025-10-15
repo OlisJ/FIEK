d020821bade16d9a99c3c595a0a3e06facc435a9
OlisJ <olisj794@gmail.com>
2025-10-15 15:39:27 +0200

    .

---
+++ Ligjerata/Java5/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a=3.5  ,  b=2;
+    float  c =a+b;
+    if (c*2<11)
+    {
+        cout<<c-1<<endl;
+    }else{
+        cout<<++b<<endl;
+    }
+    
+
+
+
+    return 0;
+}
