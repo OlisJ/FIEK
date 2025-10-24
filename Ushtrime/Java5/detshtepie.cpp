168cb8ed7044966bc074174ecdfbdba2dfac786d
OlisJ <olisj794@gmail.com>
2025-10-24 10:20:17 +0200

    detyreshtepie

--- Ushtrime/Java5/detshtepie.cpp
+++ Ushtrime/Java5/detshtepie.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int a=0;
-    int b=1;
-    int c=a+b;
+int main() {
+    int a = 0;
+    int b = 1;
+    int c;
     int n;
+    
     cout<<"Vendos n: ";
     cin>>n;
 
-
-
-
-
-
-
+    cout<<a<<endl; 
+    cout<<b<<endl;
+    
+    while (b <= n){
+        c=a+b;
+        if (c > n) break;
+        cout<<c<< endl;
+        a = b;
+        b = c;
+    }
+    
     return 0;
 }
